ed7205e91755bde3e1ec207a515cbd2972e7635c
NanUshio <120080941+NanUshio@users.noreply.github.com>
2023-02-23 15:49:40 +0800

    major src update, modified project information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'VVenC_Read_Doc'
+copyright = '2023, NanUshio'
+author = 'NanUshio'
 
 release = '0.1'
 version = '0.1.0'
